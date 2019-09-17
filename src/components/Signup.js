703a3f81396205906825586f1e2ea772b669bfac
blainelawson <blaine.lawson@gmail.com>
2019-09-17 10:16:24 -0400

    commented out signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,16 +1,38 @@
-import React from 'react'
+// import React from 'react'
+// import {connect} from 'react-redux'
+// import {updateSignupForm} from "../actions/updateSignupForm"
+// import {signup} from "../actions/currentUser"
 
-export default class Signup extends React.Component {
-    render(){
-        return(
-            <div style={{
-                position: "relative",
-                top: "50px",
-                backgroundColor: "red"
+// export default class Signup extends React.Component {
+
+//     handleInputChange = event => {
+//         const {name, value} = event.target
+//         const updatedFormInfo = {
+//             ...signupFormData,
+//             [name]: value
+//         }
+
+//         updateSignupForm(updatedFormInfo)
+//     }
+
+//     handleSubmit = event => {
+//         event.preventDefault()
+//         signup(signupFormData)
+//     }
+
+
+//     render(){
+//         return(
+//             <div style={{
+//                 position: "relative",
+//                 top: "50px",
+//                 backgroundColor: "red"
                 
-            }}>
-                <h1>SignupComponent</h1>
-            </div>
-        )
-    }
-}
+//             }}>
+//                 <h1>SignupComponent</h1>
+//             </div>
+//         )
+//     }
+// }
+
+// export default connect()(Signup)
